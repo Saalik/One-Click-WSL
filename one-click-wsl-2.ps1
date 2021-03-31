b89dccc4dd4e9a070cd53ca5c80567ba5c339273
saalik <saalik.hatia@outlook.com>
2021-03-31 11:08:02 +0200

    First commit, with minimal installation

---
+++ one-click-wsl-2.ps1
@@ -0,0 +1,28 @@
+﻿echo "Running command : Enabling Microsoft-Windows-Subsystem-Linux"
+
+Start-Process wpr -verb runAs -Args "dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart"
+
+echo "Done"
+
+echo "Running command : Enabling VirtualMachinePlatform"
+
+Start-Process wpr -verb runAs -Args "dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart"
+
+echo "Done"
+
+echo "Downloading and launching wsl update package"
+
+Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -OutFile "C:\Users\$env:UserName\AppData\Local\Temp\wsl.msi"
+Invoke-Item "C:\Users\$env:UserName\AppData\Local\Temp\wsl.msi"
+
+echo "Done"
+
+
+echo "Setting up wsl version 2 as default"
+
+Start-Process wpr -verb runAs -Args "wsl --set-default-version 2"
+
+echo "Done"
+
+
+read-host "Press any key to finish..."
